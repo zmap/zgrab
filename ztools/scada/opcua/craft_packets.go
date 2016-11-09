b740ed6dff5eed3a69762896bef0ec4f6672dc0b
holsandre <andre.holl@t-online.de>
2016-11-09 07:18:19 +0100

    Update craft_packets.go

--- ztools/scada/opcua/craft_packets.go
+++ ztools/scada/opcua/craft_packets.go
@@ -5,14 +5,14 @@ import (
 )
 // hello packet in hex
 var hexhel string = "48454c" +				// Message Type = Hel
-					"46" +					// Chunk Type = F = Final
-					"3e000000" +			// Message Size = 62
-					"00000000" +			// Proto Version = 0
-					"00000100" +			// ReceivedBufferSize = 65636
-					"00000100" +			// SendBufferSize = 65636
-					"00000001" +			// MaxMsgSize = 167
-					"88130000" +			// MaxChunkCount = 5000
-					"00000000"				// Endpoint URL = OPC Null String
+		    "46" +				// Chunk Type = F = Final
+		    "3e000000" +			// Message Size = 62
+		    "00000000" +			// Proto Version = 0
+		    "00000100" +			// ReceivedBufferSize = 65636
+		    "00000100" +			// SendBufferSize = 65636
+		    "00000001" +			// MaxMsgSize = 167
+		    "88130000" +			// MaxChunkCount = 5000
+	            "00000000"				// Endpoint URL = OPC Null String
 
 // function to build hello ; calculates message size if endpoint url is changed in the future
 func BuildHel() ([]byte,error){
@@ -22,28 +22,28 @@ func BuildHel() ([]byte,error){
 
 // same for open secure channel request
 var hexoscr string = "4f504e" + 			// Message Type = OPN
-					 "46" +					// Chunk Type = F = Final
-					 "85000000" +			// Message Size = 133
-					 "00000000" +			// Secure Channel ID
-					 "2f000000687474703a2f2f6f7063666f756e646174696f6e2e6f72672f55412f5365637572697479506f6c696379234e6f6e65" + // Length.ULInt32|Security Policy Uri
-					 "ffffffff" +			// Sender Cert
-					 "ffffffff" +			// Receiver Cert
-					 "33000000" +			// Seq Nr
-					 "03000000" +			// Request ID
-					 "0100be01" +			// Enc mask = Open Secure Channel Request
-					 "0000" + 				// Authentikation Token
-					 "08cb34bced0cd201" +	// Timestamp (only for diagnostic purpose)
-					 "00000000"	+			// Request Handle
-					 "00000000" +			// Return Diagnostics
-					 "ffffffff" +			// Audit Entry ID
-					 "00000000" +			// Timeout Hint
-					 "000000"	+			// Additional Header
-					 "00000000" +			// Client Protocol Version
-					 "00000000" +			// Security Token Request Type 
-					 "01000000" +			// Message Security Mode = 1 = None
-					 "01000000" +			// Some Padding ????
-					 "00" +					// Client Nonce 00
-					 "e0930400" 			// Requested Lifetime
+		     "46" +				// Chunk Type = F = Final
+		     "85000000" +			// Message Size = 133
+		     "00000000" +			// Secure Channel ID
+		     "2f000000687474703a2f2f6f7063666f756e646174696f6e2e6f72672f55412f5365637572697479506f6c696379234e6f6e65" + // Length.ULInt32|Security Policy Uri
+		     "ffffffff" +			// Sender Cert
+		     "ffffffff" +			// Receiver Cert
+		     "33000000" +			// Seq Nr
+		     "03000000" +			// Request ID
+		     "0100be01" +			// Enc mask = Open Secure Channel Request
+		     "0000" + 				// Authentikation Token
+		     "08cb34bced0cd201" +		// Timestamp (only for diagnostic purpose)
+		     "00000000"	+			// Request Handle
+		     "00000000" +			// Return Diagnostics
+		     "ffffffff" +			// Audit Entry ID
+		     "00000000" +			// Timeout Hint
+		     "000000"	+			// Additional Header
+		     "00000000" +			// Client Protocol Version
+		     "00000000" +			// Security Token Request Type 
+		     "01000000" +			// Message Security Mode = 1 = None
+		     "01000000" +			// Some Padding ????
+		     "00" +				// Client Nonce 00
+		     "e0930400" 			// Requested Lifetime
 
 // same as hello
 func BuildOscr()([]byte,error){
@@ -53,17 +53,17 @@ func BuildOscr()([]byte,error){
 
 // Find Servers Request
 var  hexfsr string = "4d5347" + 			// Message Type = MSG
-					 "46"	  +				// Chunk Type = F = Final
-					 "63000000" +			// Message Size = 99
-					 "ec8d0000" +			// Secure Channel ID
-					 "01000000" +			// Security Token ID
-					 "00000000" +			// Security Sequence Number = 0
-					 "01000000" +			// Security Request ID
-					 "0100a601" +			// Expand Node ID
-					 "000008cb34bced0cd2010000000000000000ffffffff10270000000000" + // Request Header w timestamp & stuff
-					 //"1e0000006f70632e7463703a2f2f3139322e3136382e3235352e34383a3438303230" + // Length.ULInt32|Endpoint URL
-					 "00000000000000000000000000000000000000000000000000000000000000000000" +  // Endpoint URL OPCUA Empty String
-					 "0000000000000000"
+		     "46"     +				// Chunk Type = F = Final
+		     "63000000" +			// Message Size = 99
+		     "ec8d0000" +			// Secure Channel ID
+		     "01000000" +			// Security Token ID
+		     "00000000" +			// Security Sequence Number = 0
+		     "01000000" +			// Security Request ID
+		     "0100a601" +			// Expand Node ID
+		     "000008cb34bced0cd2010000000000000000ffffffff10270000000000" + // Request Header w timestamp & stuff
+		     //"1e0000006f70632e7463703a2f2f3139322e3136382e3235352e34383a3438303230" + // Length.ULInt32|Endpoint URL
+		     "00000000000000000000000000000000000000000000000000000000000000000000" +  // Endpoint URL OPCUA Empty String
+		     "0000000000000000"
 
 // build find servers ; Secure Channel ID is included
 func BuildFsr(SecChanId string)([]byte,error){
@@ -73,17 +73,17 @@ func BuildFsr(SecChanId string)([]byte,error){
 
 // get endpoints request
 var hexger string = "4d5347"  +				// Message Type = MSG
-					"46" +					// Chunk Type = F = Final
-					"63000000" +			// Message Size = 99
-					"ec8d0000" +			// Secure Channel ID
-					"01000000" +			// Security Token ID
-					"34000000" +			// Security Sequence Number = 0
-					"02000000" +			// Security Request ID
-					"0100ac01" +			// Expand Node ID
-					"000008cb34bced0cd2010100000000000000ffffffff10270000000000" + // Request Header w timestamp & stuff
-					//"1e0000006f70632e7463703a2f2f3139322e3136382e3235352e34383a3438303230" +  // Length.ULInt32|Endpoint URL
-					"00000000" +  // Endpoint URL OPCUA Empty String
-					"0000000000000000"
+		    "46" +					// Chunk Type = F = Final
+		    "63000000" +			// Message Size = 99
+		    "ec8d0000" +			// Secure Channel ID
+		    "01000000" +			// Security Token ID
+		    "34000000" +			// Security Sequence Number = 0
+		    "02000000" +			// Security Request ID
+		    "0100ac01" +			// Expand Node ID
+		    "000008cb34bced0cd2010100000000000000ffffffff10270000000000" + // Request Header w timestamp & stuff
+		   //"1e0000006f70632e7463703a2f2f3139322e3136382e3235352e34383a3438303230" +  // Length.ULInt32|Endpoint URL
+		    "00000000" +  // Endpoint URL OPCUA Empty String
+		    "0000000000000000"
 
 // build ger, Secure Channel ID + Security Token ID included
 func BuildGer(SecChanId string,SecTokenId string)([]byte,error){
