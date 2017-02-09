d9a4de9958bd6aea8961f8a9f65d552eca4beb4f
Alex Holland <ahollan@umich.edu>
2017-02-09 12:29:37 -0500

    Removed original mod

--- zlib/conn.go
+++ zlib/conn.go
@@ -429,11 +429,6 @@ func (c *Conn) SMTPBanner(b []byte) (int, error) {
 }
 
 func (c *Conn) EHLO(domain string) error {
-	if domain == "" {
-		localAddr := c.LocalAddr().String()
-		idx := strings.LastIndex(localAddr, ":")
-		domain = localAddr[0:idx]
-	}
 	cmd := []byte("EHLO " + domain + "\r\n")
 	if _, err := c.getUnderlyingConn().Write(cmd); err != nil {
 		return err
